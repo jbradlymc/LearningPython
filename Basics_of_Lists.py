7b11f96798bfc452a9e2d6f00b28cb54d61f1687
JoshBradly <joshbradly16@gmail.com>
2025-12-29 00:14:04 +0800

    added new problem from Cisco Networking Academy

---
+++ Basics_of_Lists.py
@@ -0,0 +1,10 @@
+hat_list = [1, 2, 3, 4, 5]
+
+hat_list[2] = int(
+    input("Enter a number to replace the middle element in the list: "))
+
+del hat_list[-1]
+
+print(len(hat_list))
+
+print(hat_list)
